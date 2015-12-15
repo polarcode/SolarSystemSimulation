9e89c2317380a55d4492ca9d8b64a9689fd0ee5b
derPinguin <derPinguin@users.noreply.github.com>
2015-12-15 19:17:06 +0100

    some more refactoring of code

--- sources/init.m
+++ sources/init.m
@@ -1,67 +1,61 @@
+% === init view ===
 clear;
 figure(1);
 title('Solar System');
-xlabel('X');
-ylabel('Y');
-zlabel('Z');
 hold on;
-grid on;
-axis vis3d;
-%format long
-axis([-30, 30, -30, 30, -30, 30])
-axis off
-orbitAng = 1;
 
+% Axis configuration
+xlabel('X'); ylabel('Y'); zlabel('Z');
+axis([-100, 100, -100, 100, -100, 100]);
+axis off;
+axis vis3d;
+
+% Background
 % This creates the 'background' axes
-ha = axes('units','normalized', ...
-            'position',[0 0 1 1]);
+ha = axes('units','normalized', 'position',[0 0 1 1]);
 % Move the background axes to the bottom
 uistack(ha,'bottom');
 % Load in a background image and display it using the correct colors
-I=imread('imgs/SpaceBackground.png');
-hi = imagesc(I)
+hi = imagesc(imread('imgs/SpaceBackground.png'));
 set(hi,'alphadata',.8)
 % Turn the handlevisibility off so that we don't inadvertently plot into the axes again
 % Also, make the axes invisible
-set(ha,'handlevisibility','off', ...
-            'visible','off')
+set(ha,'handlevisibility','off', 'visible','off')
 
-resolution = 70;
-
-%Sun light
+% Sun light
 light('Position',[0 0 0],'Style','local');
 lighting flat
 %lighting gouraud
 
-%material shiny
-material dull
-%material metal
-
-shading interp
+% === start ===
+rotation_angle = 1; % a.k.a. speed
+resolution = 70;
 
 [sun, earth, saturn, saturn_ring] = getUniverse(resolution);
 
+% This kind of sucks...
+% it'd be nice if we could get this information from the surface object
 satPos = [0,0,20;...
           0,0,0;...
           0,0,0];
 
-while 1
-    rotate(earth, [0,0,1], 10, [0,0,0])
+M_rot = [cosd(rotation_angle) -sind(rotation_angle) 0;...
+        sind(rotation_angle) cosd(rotation_angle) 0;...
+        0 0 1];
 
+% Run simulation
+while 1
     % Orbit
-    rotate(saturn, [0,0,1], orbitAng, [0,0,0])
-    rotate(saturn_ring, [0,0,1], orbitAng, [0,0,0])
+    rotate(earth, [0,0,1], 10, [0,0,0])
+    
+    rotate(saturn, [0,0,1], rotation_angle, [0,0,0])
+    rotate(saturn_ring, [0,0,1], rotation_angle, [0,0,0])
     
     % Spin / Rotation
-    Mr_saturn = [cosd(orbitAng) -sind(orbitAng) 0;...
-                sind(orbitAng) cosd(orbitAng) 0;...
-                0 0 1];
-    satPos = Mr_saturn * satPos;
-    
-    rotate(saturn_ring, [0,0,1], -orbitAng, [satPos(1,3), satPos(2,3), satPos(3,3)])
-    %pause(0.001);
+    satPos = M_rot * satPos;
+    rotate(saturn_ring, [0,0,1], -rotation_angle, [satPos(1,3), satPos(2,3), satPos(3,3)])
+
     drawnow;
-    %pause(100);
 end
 
   
