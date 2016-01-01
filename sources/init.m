5921124083ef0eae7326d851ac421679f5152bff
derPinguin <derPinguin@users.noreply.github.com>
2016-01-01 21:57:24 +0100

    adding moon

--- sources/init.m
+++ sources/init.m
@@ -28,26 +28,34 @@ light('Position',[0 0 0],'Style','local');
 lighting flat
 
 % === start ===
-speed = 10;
+speed = 1;
 rotation = 1;
 resolution = 70;
 AU = 10; % definition of one astronomical unit (distance earth to sun)
 radius = 10; % scale of size of planets (log10)
 
-[sun, mercury, venus, earth, mars, jupiter, saturn, saturn_ring, uranus, neptune] = getUniverse(AU, radius, resolution);
+[sun, mercury, venus, earth, moon, mars, jupiter, saturn, saturn_ring, uranus, neptune] = getUniverse(AU, radius, resolution);
 
 material dull
 shading interp
 
 % This kind of sucks...
 % it'd be nice if we could get this information from the surface object
-satPos = [0,0,9.58*AU;...
+pos_saturn = [0,0,9.58*AU;...
           0,0,0;...
           0,0,0];
 
-M_rot_sat = [cosd((1/29.45)*speed) -sind((1/29.45)*speed) 0;...
-        sind((1/29.45)*speed) cosd((1/29.45)*speed) 0;...
-        0 0 1];
+pos_earth = [0,0,AU;...
+          0,0,0;...
+          0,0,0];
+
+M_rot_sat = [cosd(0.034*speed) -sind(0.034*speed) 0;...
+            sind(0.034*speed) cosd(0.034*speed) 0;...
+            0 0 1];
+
+M_rot_earth = [cosd(speed) -sind(speed) 0;...
+            sind(speed) cosd(speed) 0;...
+            0 0 1];
 
 % Run simulation
 while 1
@@ -60,12 +68,17 @@ while 1
     
     rotate(saturn, [0,0,1], 0.034*speed, [0,0,0])
     rotate(saturn_ring, [0,0,1], 0.034*speed, [0,0,0])
-    
+
     % Spin / Rotation
-    satPos = M_rot_sat * satPos;
-    rotate(saturn_ring, [0,0,1], -0.034*rotation, [satPos(1,3), satPos(2,3), satPos(3,3)])
+    pos_saturn = M_rot_sat * pos_saturn;
+    rotate(saturn_ring, [0,0,1], -0.034*rotation, [pos_saturn(1,3), pos_saturn(2,3), pos_saturn(3,3)])
 
     rotate(uranus, [0,0,1], 0.0119*speed, [0,0,0])
     rotate(neptune, [0,0,1], 0.0061*speed, [0,0,0])
+
+    % Moons
+    pos_earth = M_rot_earth * pos_earth;
+    rotate(moon, [0,0,1], 13.3795*speed, [pos_earth(1,3), pos_earth(2,3), pos_earth(3,3)])
+
     drawnow;
 end
